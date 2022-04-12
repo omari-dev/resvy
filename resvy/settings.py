842f613707704c2019a3651259864158cd6d774e
Omar Alomari <i.omary@hotmail.com>
2022-04-12 23:34:24 +0300

    adding docker ignore entry

--- resvy/settings.py
+++ resvy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-__yxu=*+62c*g_yh-s%e5f1t!)den7bm-+ru++=&9a=4gmz!a='
+SECRET_KEY = 'django-insecure-__yxu=*+62c*g_yh-s%e5f1t!)den7bm-+ru++=&9a=4gmz!a='  # TODO: read it from env variable
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
